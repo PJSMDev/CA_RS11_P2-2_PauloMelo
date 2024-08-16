9f00cd12d48fb00bc8242c749a01115f1fede3ae
PJSM <pjsm.lesi@gmail.com>
2024-08-16 23:21:01 +0100

    Created controllers via scaffolding

---
+++ ClientManagement/Controllers/ClientController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ClientManagement.DAL;
+using ClientManagement.Models;
+
+namespace ClientManagement.Controllers
+{
+    public class ClientController : Controller
+    {
+        private readonly ClientManagementDbContext _context;
+
+        public ClientController(ClientManagementDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Client
+        public async Task<IActionResult> Index()
+        {
+            var clientManagementDbContext = _context.Clients.Include(c => c.MembershipType);
+            return View(await clientManagementDbContext.ToListAsync());
+        }
+
+        // GET: Client/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Clients == null)
+            {
+                return NotFound();
+            }
+
+            var client = await _context.Clients
+                .Include(c => c.MembershipType)
+                .FirstOrDefaultAsync(m => m.ClientId == id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            return View(client);
+        }
+
+        // GET: Client/Create
+        public IActionResult Create()
+        {
+            ViewData["MembershipTypeId"] = new SelectList(_context.MembershipTypes, "MembershipTypeId", "MembershipTypeId");
+            return View();
+        }
+
+        // POST: Client/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ClientId,Name,Email,DateOfBirth,PhoneNumber,MembershipTypeId,IsLoyal")] Client client)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(client);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MembershipTypeId"] = new SelectList(_context.MembershipTypes, "MembershipTypeId", "MembershipTypeId", client.MembershipTypeId);
+            return View(client);
+        }
+
+        // GET: Client/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Clients == null)
+            {
+                return NotFound();
+            }
+
+            var client = await _context.Clients.FindAsync(id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+            ViewData["MembershipTypeId"] = new SelectList(_context.MembershipTypes, "MembershipTypeId", "MembershipTypeId", client.MembershipTypeId);
+            return View(client);
+        }
+
+        // POST: Client/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ClientId,Name,Email,DateOfBirth,PhoneNumber,MembershipTypeId,IsLoyal")] Client client)
+        {
+            if (id != client.ClientId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(client);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ClientExists(client.ClientId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MembershipTypeId"] = new SelectList(_context.MembershipTypes, "MembershipTypeId", "MembershipTypeId", client.MembershipTypeId);
+            return View(client);
+        }
+
+        // GET: Client/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Clients == null)
+            {
+                return NotFound();
+            }
+
+            var client = await _context.Clients
+                .Include(c => c.MembershipType)
+                .FirstOrDefaultAsync(m => m.ClientId == id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            return View(client);
+        }
+
+        // POST: Client/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Clients == null)
+            {
+                return Problem("Entity set 'ClientManagementDbContext.Clients'  is null.");
+            }
+            var client = await _context.Clients.FindAsync(id);
+            if (client != null)
+            {
+                _context.Clients.Remove(client);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ClientExists(int id)
+        {
+          return (_context.Clients?.Any(e => e.ClientId == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ ClientManagement/Controllers/PaymentController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ClientManagement.DAL;
+using ClientManagement.Models;
+
+namespace ClientManagement.Controllers
+{
+    public class PaymentController : Controller
+    {
+        private readonly ClientManagementDbContext _context;
+
+        public PaymentController(ClientManagementDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Payment
+        public async Task<IActionResult> Index()
+        {
+            var clientManagementDbContext = _context.Payments.Include(p => p.Client);
+            return View(await clientManagementDbContext.ToListAsync());
+        }
+
+        // GET: Payment/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Payments == null)
+            {
+                return NotFound();
+            }
+
+            var payment = await _context.Payments
+                .Include(p => p.Client)
+                .FirstOrDefaultAsync(m => m.PaymentId == id);
+            if (payment == null)
+            {
+                return NotFound();
+            }
+
+            return View(payment);
+        }
+
+        // GET: Payment/Create
+        public IActionResult Create()
+        {
+            ViewData["ClientId"] = new SelectList(_context.Clients, "ClientId", "Email");
+            return View();
+        }
+
+        // POST: Payment/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("PaymentId,ClientId,Amount")] Payment payment)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(payment);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClientId"] = new SelectList(_context.Clients, "ClientId", "Email", payment.ClientId);
+            return View(payment);
+        }
+
+        // GET: Payment/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Payments == null)
+            {
+                return NotFound();
+            }
+
+            var payment = await _context.Payments.FindAsync(id);
+            if (payment == null)
+            {
+                return NotFound();
+            }
+            ViewData["ClientId"] = new SelectList(_context.Clients, "ClientId", "Email", payment.ClientId);
+            return View(payment);
+        }
+
+        // POST: Payment/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("PaymentId,ClientId,Amount")] Payment payment)
+        {
+            if (id != payment.PaymentId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(payment);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PaymentExists(payment.PaymentId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClientId"] = new SelectList(_context.Clients, "ClientId", "Email", payment.ClientId);
+            return View(payment);
+        }
+
+        // GET: Payment/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Payments == null)
+            {
+                return NotFound();
+            }
+
+            var payment = await _context.Payments
+                .Include(p => p.Client)
+                .FirstOrDefaultAsync(m => m.PaymentId == id);
+            if (payment == null)
+            {
+                return NotFound();
+            }
+
+            return View(payment);
+        }
+
+        // POST: Payment/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Payments == null)
+            {
+                return Problem("Entity set 'ClientManagementDbContext.Payments'  is null.");
+            }
+            var payment = await _context.Payments.FindAsync(id);
+            if (payment != null)
+            {
+                _context.Payments.Remove(payment);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PaymentExists(int id)
+        {
+          return (_context.Payments?.Any(e => e.PaymentId == id)).GetValueOrDefault();
+        }
+    }
+}
