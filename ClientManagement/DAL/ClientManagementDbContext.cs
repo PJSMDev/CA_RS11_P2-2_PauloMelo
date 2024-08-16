587959a5f36060852d88023e71f2df6dffbda5f9
PJSM <pjsm.lesi@gmail.com>
2024-08-16 20:29:26 +0100

    Fixed appsettings.json, Implemented forgotten Payment.cs model

---
+++ ClientManagement/DAL/ClientManagementDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ClientManagement.Models;
+
+namespace ClientManagement.DAL
+{
+    /// <summary>
+    /// Represents the database context for the application.
+    /// </summary>
+    public class ClientManagementDbContext : DbContext
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ClientManagementDbContext"/> class.
+        /// </summary>
+        /// <param name="options">The options to be used by the <see cref="DbContext"/>.</param>
+        public ClientManagementDbContext(DbContextOptions<ClientManagementDbContext> options)
+            : base(options)
+        {
+        }
+
+        // DbSets for each entity in the model
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<MembershipType> MembershipTypes { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<ClientService> ClientServices { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+
+        /// <summary>
+        /// Configures the model creating process.
+        /// </summary>
+        /// <param name="modelBuilder">The model builder.</param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Additional configurations can be placed here
+        }
+    }
+}
---
+++ ClientManagement/Models/Payment.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ClientManagement.Models
+{
+    /// <summary>
+    /// Represents a payment made by a client.
+    /// </summary>
+    public class Payment
+    {
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the unique identifier for the payment.
+        /// </summary>
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int PaymentId { get; set; } // Primary key
+
+        /// <summary>
+        /// Gets or sets the unique identifier of the client who made the payment.
+        /// </summary>
+        [Required]
+        public int ClientId { get; set; } // Foreign key to Client
+
+        /// <summary>
+        /// Gets or sets the amount of money paid.
+        /// </summary>
+        [Required]
+        [DataType(DataType.Currency)]
+        public decimal Amount { get; set; }
+
+        #endregion
+
+        #region Navigational Properties
+
+        /// <summary>
+        /// Gets or sets the client who made the payment.
+        /// </summary>
+        public Client Client { get; set; }
+
+        #endregion
+    }
+}
